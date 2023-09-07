70ea6c62fa27046d5cd6fda1b2ce867357ac155b
goldietiara <goldietiara.acc@gmail.com>
2023-09-07 22:27:05 +0700

    ..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default async function Home({
   searchParams: { category, endCursor },
 }: typeProps) {
   const data = (await fetchAllProjects(
-    category,
+    "Frontend",
     endCursor
   )) as typeProjectSearch;
 
