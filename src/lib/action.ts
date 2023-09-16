a73100d31f8b2b5abf61aa27bba388341416fe85
goldietiara <goldietiara.acc@gmail.com>
2023-09-16 16:09:46 +0700

    bugs on uploading to cloudinary

--- src/lib/action.ts
+++ src/lib/action.ts
@@ -15,18 +15,15 @@ import { ProjectForm } from "@/common.types";
 // Communicate with Back-End
 
 const isProduction = process.env.NODE_ENV === "production";
-
 const apiUrl = isProduction
   ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || ""
   : "http://127.0.0.1:4000/graphql";
-
 const apiKey = isProduction
   ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || ""
-  : "let me in...";
-
+  : "letmein";
 const serverUrl = isProduction
   ? process.env.NEXT_PUBLIC_SERVER_URL
-  : "http://localhost:3000/";
+  : "http://localhost:3000";
 
 const client = new GraphQLClient(apiUrl);
 
